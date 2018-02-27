d51341a546edb7ad1a9c9af72ea23c61f9d7b51d
Dante Spencer <dantespe@umich.edu>
2018-02-27 00:01:43 -0800

    Add floor divide test cases

--- tests.py
+++ tests.py
@@ -7,13 +7,20 @@ class TestBasics(unittest.TestCase):
     def test_add(self):
         result = rpn.calculate("1 1 +", output=False)
         self.assertEqual(2, result)
+
     def test_subtract(self):
         result = rpn.calculate("5 3 -", output=False)
         self.assertEqual(2, result,)
+
     def test_multiply(self):
         result = rpn.calculate("5 3 *", output=False)
         self.assertEqual(15, result)
-    def test_divide(self):
+
+    def test_true_divide(self):
         result = rpn.calculate("6 3 /", output=False)
         self.assertEqual(2, result)
+
+    def test_floor_divide(self):
+        self.assertEqual(2, rpn.calculate("9 4 //", output=False))
+
 
