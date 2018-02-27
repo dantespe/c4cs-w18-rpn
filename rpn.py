bf71c95707bbdff75444e0a71f61db5876c29f43
Dante Spencer <dantespe@umich.edu>
2018-02-27 08:40:46 -0800

    Refactor to add binary and unary operator support

--- rpn.py
+++ rpn.py
@@ -1,16 +1,41 @@
 #!/usr/bin/env python3
 
 import operator
-
+import extras_operators
 
 operators = {
-    '+': operator.add,
-    '-': operator.sub,
-    '*': operator.mul,
-    '/': operator.truediv,
-    '//': operator.floordiv,
-    '^': operator.pow,
-    '&': operator.and_,
+    '+': {
+        'function': operator.add,
+        'type': "binary",
+    },
+    '-': {
+        'function': operator.sub,
+        'type': 'binary',
+    },
+    '*': {
+        'function': operator.mul,
+        'type': 'binary'
+    },
+    '/': {
+        'function': operator.truediv,
+        'type': 'binary'
+    },
+    '//': {
+        'function': operator.floordiv,
+        'type': 'binary'
+    },
+    '^': {
+        'funciton': operator.pow,
+        'type': 'binary'
+    },
+    '&': {
+        'function': operator.and_,
+        'type': 'binary',
+    },
+    '|': {
+        'function': operator.or_,
+        'type': 'binary',
+    }
 }
 
 def calculate(myarg, output=True):
@@ -19,12 +44,17 @@ def calculate(myarg, output=True):
         try:
             token = int(token)
             stack.append(token)
+
         except ValueError:
-            function = operators[token]
+            function = operators[token]['function']
             arg2 = stack.pop()
-            arg1 = stack.pop()
-            result = function(arg1, arg2)
-            stack.append(result)
+
+            if operators[token]['type'] == 'binary':
+                arg1 = stack.pop()
+                stack.append(function(arg1, arg2))
+            else:
+                stack.append(function(arg2))
+
         if output:
             print(stack)
     if len(stack) != 1:
